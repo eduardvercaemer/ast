72698acaa801ed782ac3cbf9cf5134dc5ae91b20
ed <vercaemereduard@gmail.com>
2024-04-13 10:19:02 -0600

    wip

--- egs/ASVspoof21/test_model.py
+++ egs/ASVspoof21/test_model.py
@@ -13,8 +13,10 @@ sys.path.append(parentdir)
 
 import models
 import torch
+import torchaudio
 
-model_path = './exp/test-asvspoof21-f10-t10-impTrue-aspTrue-b32-lr1e-5/fold5/models/best_audio_model.pth'
+
+model_path = './data/models/model-5.pth'
 input_tdim = 512 # same as run_asvspoof21.sh audio_length
 
 # initialize an AST model
@@ -33,7 +35,36 @@ audio_model = models.ASTModel(
 audio_model = torch.nn.DataParallel(audio_model)
 audio_model.load_state_dict(sd, strict=False)
 
-# input a batch of 10 spectrogram, each with 100 time frames and 128 frequency bins
-test_input = torch.rand([10, input_tdim, 128])
-test_output = audio_model(test_input)
-print(test_output.shape)
+#input a batch of 10 spectrogram, each with 100 time frames and 128 frequency bins
+# test_input = torch.rand([10, input_tdim, 128])
+# test_input = test_input.to(device)
+# test_output = audio_model(test_input)
+# print(test_output.shape)
+
+# spoof wav
+waveform, sample_rate = torchaudio.load('./data/ASVspoof2021_DF_eval/wav/DF_E_2000011.wav')
+
+import torchaudio.transforms as T
+import torch.nn.functional as F
+
+# Define transformation
+transform = T.MelSpectrogram(sample_rate=sample_rate, n_fft=400, hop_length=160, n_mels=128)
+
+# Apply transformation to waveform
+spectrogram = transform(waveform)
+
+# Interpolate the spectrogram tensor to match model's input size
+spectrogram = F.interpolate(input=spectrogram.unsqueeze(0), size=[input_tdim, 128], mode='nearest')
+
+# Adjust shape for correct model input
+input_waveform = spectrogram.transpose(1, 2)
+
+# Just take the first item of the batch
+input_waveform = input_waveform[0].unsqueeze(0)
+
+# Use the squeeze method to remove the singular dimension from the tensor
+input_waveform = input_waveform.squeeze(2)
+
+output = audio_model(input_waveform)
+
+print(output)
